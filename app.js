4038ff7e4de183a8448622818f93eb37655563e2
Navnit Raj <anit94u@gmail.com>
2021-08-17 21:34:08 +0530

    prepare for deployment

--- app.js
+++ app.js
@@ -71,7 +71,7 @@ app.use(errorController.get404);
 mongoose
   .connect(MONGO_DB_URI)
   .then(result => {
-    app.listen(3000);
+    app.listen(process.env.PORT || 3000);
   })
   .catch(err => {
     console.log(err);
