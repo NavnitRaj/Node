5393f680f911c7206f5aa7e0bb0bd1f9d4b70cc4
Navnit Raj <anit94u@gmail.com>
2021-08-19 09:07:29 +0530

    email and Advance authentication integration

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,14 @@
 const Session = require('../models/session');
 const User = require('../models/user');
 const bcrypt = require('bcryptjs');
-
+const nodemailer = require('nodemailer');
+const sendGridTransport = require('nodemailer-sendgrid-transport');
+const crypto = require('crypto');
+const transporter = nodemailer.createTransport(sendGridTransport({
+  auth: {
+    api_key: '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',
+  }
+}));
 exports.getLogin = (req, res, next) => {
   if(req.session.isLoggedIn){
     return res.redirect('/');
@@ -87,6 +94,14 @@ exports.postSignup = (req, res, next) => {
         return user.save();
       })
       .then(result => {
+        return transporter.sendMail({
+          to: email,
+          from: 'nraj011994@outlook.com',
+          subject: 'Signup Successfully',
+          html: '<h1>You are Successfully signed up!</h1><br/><p>Your ID: '+email+'<br/> Your Password: '+password+'</p>'
+        })
+      })
+      .then(result => {
         req.flash('info','Registered Successfully');
         return res.redirect('/login');
       })
@@ -106,3 +121,67 @@ exports.postLogout = (req, res, next) => {
     res.redirect('/');
   });
 };
+
+exports.getReset = (req, res, next) => {
+  res.render('auth/reset', {
+    path: '/reset',
+    pageTitle: 'Reset Password',
+  });
+}
+exports.postReset = (req,res,next) => {
+  const email = req.body.email;
+  const host = req.headers.host
+  crypto.randomBytes(32, (err, buffer) => {
+    if(err){
+      console.log(err)
+      return res.redirect('/reset')
+    }
+    const token = buffer.toString('hex');
+    User.findOne({email: req.body.email})
+    .then(user => {
+      if(!user){
+        res.flash('error', 'No User Found');
+        return res.redirect('/reset');
+      }
+      user.resetToken = token;
+      user.resetTokenExpiry = Date.now() + 600000;
+      return user.save();
+    })
+    .then(result => {
+      res.redirect('/');
+      return transporter.sendMail({
+        to: req.body.email,
+        from: 'nraj011994@outlook.com',
+        subject: 'Reset Your Password',
+        html: 
+        `
+        <p>You requested a password reset</p>
+        <p><a href="http://${host}/reset/${token}/${email}">Click me reset password</a><p>
+        `
+      })
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  })
+}
+exports.getNewPassword= (req, res, next) => {
+  const token = req.params.token;
+  const email = req.params.email;
+  User.findOne({email:email, resetToken:token})
+  .then(user => {
+    if(!user){
+      req.flash('error','Invalid Token');
+      return res.redirect('/login');
+    }
+    if(user.resetTokenExpiry < Date.now()){
+      req.flash('error','Ops! Token Expired');
+      return res.redirect('/login');
+    }
+    res.render('auth/new-password',{
+      path: '/reset',
+      pageTitle: 'Reset Password',
+    });
+  })
+  .catch(err=> console.log(err))
+}
--- routes/auth.js
+++ routes/auth.js
@@ -8,9 +8,15 @@ router.get('/login', authController.getLogin);
 
 router.get('/signup', authController.getSignup);
 
+router.get('/reset', authController.getReset);
+
+router.get('/reset/:token/:email', authController.getNewPassword);
+
 router.post('/login', authController.postLogin);
 
 router.post('/signup', authController.postSignup);
+
+router.post('/reset', authController.postReset);
 
 router.post('/logout', authController.postLogout);
 
